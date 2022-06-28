ad2235de3dff6bf46cef6e1215926b62540be6cf
veuna-R <veunakmr@gmail.com>
2022-06-28 16:26:44 +0200

    updated 28/06/2022 @16:26

---
+++ poo/00-cours.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <title>Introduction - cours programmation orientée objet</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="p-3 bg-light">
+        <div class="container text-center">
+            <h1 class="display-4">POO: la Programmation Orientée Objet</h1>
+            <p class="lead">Introduction</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 mt-2">
+                <h2 class="text-center">Historique</h2>
+                <p>Le notion d'objet a été un produit dans les langages de programmation en 1962 par le langage 'simula'. Cette façcon de programmer a évoluée avant d'être popularisée en 1983 avec la sortie du langage C++, un langage orienté objet dont l'utilisation ressemble à celle du langage C.</p>
+                <p>Cette nouvelle façcon de programmer répond à plusieurs besoins:</p>
+                <ul>
+                    <li>Elle facilite les changements importatns dans le programme.</li>
+                    <li>En cas de changements dans les langages, elle facilite la mise à nouveau.</li>
+                </ul>
+                <p>La POO va permettre de faire façe aux changements majeurs qui auraient pus demander de reprendre un programme de A à Z. Mais elle va demander également une nouvelle façon de programmer et de réfléchir à l'organisation du code. 3 stratégies sont alors à mettre en place:</p>
+                <ul>
+                    <li>Modéliser différenements</li>
+                    <li>Modulariser</li>
+                    <li>Encapsuler</li>
+                </ul>
+            </div><!-- fin de la classe "col-12: Historique" -->
+
+            <div class="col-12">
+                <h2 class="text-center">Les Avantages de la POO</h2>
+                <p>Le principal avantage de la POO est de faciliter la réutilisation d'un module. Ainsi, on pourra reprendre les méthodes (fonctions) d'une classe éxistante dans une nouvelle classe grâce à la notion d'héritage.</p>
+                <p>Une autre chose facilitée par l'orienté objet est la lecture du code. En effet, on comprendra plus facilement une instruction <code>->ajout</code>, qu'un 'if else' avec des multiples conditions dans une boucle. La force de cette façcon de programmer est qu'elle se calque sur la réalité physique. Une classe sera par exemple Renault. Les objets ou instanciations de cette classe seront $voiture1, $voiture2, etc. L'objet est donc la voiture. Cette objet aura des caractéristiques représentées par des variables, appelées propriétés et des comportements représentés par des fonctions, appelées méthodes. En conclusion, les avantages de cette façon de programmer sont les suivants:</p>
+                <ul>
+                    <li>La facilitation du travail collaboratif</li>
+                    <li>La simplification de la maintenance</li>
+                    <li>L'assouplissement du code</li>
+                </ul>
+                <p>Cette approach est tout de même moins intutive que l'approche procédure. La POO oblige en effet à réfléchir et modéliser avant de programmer.</p>
+            </div><!-- fin de la classe "col-12: Avantages" -->
+
+        </div><!-- fin de la rangée -->
+    </div><!-- fin du "container" -->
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ poo/01-objet.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <title>Introduction - cours programmation orientée objet</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="p-3 bg-light">
+        <div class="container text-center">
+            <h1 class="display-4">POO: la Programmation Orientée Objet</h1>
+            <p class="lead">Introduction</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 mt-2">
+                <h2 class="text-center">Que sont des objets</h2>
+                <p>Les objets resemblent fortement aux variables que l'on connait (string, integer, float, array, etc.), mais ces dernières sont limitées et on peut se retrouvé blocqué. Par exemple, lorsque l'on crée un réseau social et que l'on veut sauvegarder un utilisateur. A ce moment là, on voudra définir ses limites, ses droits, etc.. Ces tableaux deviennent vite très complexes et si on droit les trasnmettre notre code, les personnes qui ne l'ont pas écrit s'y perdent facilement. Un autre exemple où le prcédural est compliqué à appliquer est lrosque l'on s'occupe des dates. Elles sont effet considérées comme des chaînes de caractêres et faire des ajouts de jours ou de mois dessus devient très compliqué. Il faudra convertir les donneées et passé d'un format à l'autre. En orienté objet, les manipulations de dates seront beaucoup plus simples.</p>
+                <p class="alert alert-info"><strong>MaDate</strong> est une classe <br><strong>$date1 = new MaDate;</strong> sont des objets ou bien des instancations de celle classe.</p>
+            </div><!-- fin de la classe "col-12: Historique" -->
+
+            <div class="col-12">
+                <h2 class="text-center">Les Propriétées</h2>
+                <p>Sur nos différents objets (ou instanciations) nous allons avoir des propriétés, des variables. Un objet pourra contenir plusieurs propriétés mais aussi plusieurs méthodes: sur une classe 'MaDate' on pourra imaginer des propriétés pour afficher le nombre de jours mais aussi des méthodes pour ajouter des jours ou des mois.</p>
+            </div><!-- fin de la classe "col-12: Avantages" -->
+
+            <div class="col-12">
+                <h2 class="text-center">Les Méthodes</h2>
+                <p>Comme nous en avons parlé en dessus, les classes peuvent aussi contenir des méthodes. Ces dernières sont en effet des fonctions, appelées méthode dans le cadre de la POO. Elles ont donc la même non-enclature qu'en procédural: <code>nomMethode();</code>.</p>
+            </div><!-- fin de la classe "col-12: Avantages" -->
+        
+        </div><!-- fin de la rangée -->
+    </div><!-- fin du "container" -->
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+</body>
+
+</html>
